ee024496d5f27e27932225807151c4cfe65c72dc
Aiman Saad <aimansaad84@gmail.com>
2021-01-16 21:43:15 +0500

    finalised task 5

---
+++ task 5/task 5.js
@@ -0,0 +1,5 @@
+var num = parseFloat(prompt("enter a number"));
+var numrnd = Math.round(num);
+var numflr = Math.floor(num);
+var numcil = Math.ceil(num);
+document.write("number: " + num + "<br>round off value: " + numrnd + "<br>floor value: " + numflr + "<br>ceil value: " + numcil);
