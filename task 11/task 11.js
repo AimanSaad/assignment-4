2ef6a8c7ad7aca90b503a828ee60153509de6e5e
Aiman Saad <aimansaad84@gmail.com>
2021-01-19 17:27:52 +0500

    updated task 11

---
+++ task 11/task 11.js
@@ -0,0 +1,11 @@
+var ip = prompt("enter a number");
+var sum = 0;
+var sip = ip.split("");
+for (var i = 0; i < sip.length; i++) {
+    document.write("<br>" + sip[i] );
+    sum = sum + parseInt(sip[i]);
+     var mean;
+}
+
+
+document.write("<br>"+ sum + " mean " + mean );
