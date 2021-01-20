ac377791a1e5483749b0f698aeee6bd2b013d19d
Aiman Saad <aimansaad84@gmail.com>
2021-01-20 15:50:26 +0500

    finalized task 12

--- task 11/task 11.js
+++ task 11/task 11.js
@@ -2,8 +2,8 @@
 var ip = prompt("enter a number");
 var sum = 0;
 var sip = ip.split("");
-for (var i = 0; i < sip.length; i++) {
+for (var i = 1; i < sip.length; i++) {
    sum = sum + parseInt(sip[i]);
-     var mean = sum / (i + 1);
+     var mean = sum / (i);
 }
 document.write("<br> Input "+ ip + " Output " + mean );
---
+++ task 12/task 12.js
@@ -0,0 +1,7 @@
+var date = new Date;
+if (date < 16 ) {
+    document.write("First fifteen days of the month");
+}
+else {
+    document.write("Last days of the month");
+}
